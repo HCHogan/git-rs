521f8633d02e5b70c16b6c6049c074f31276d66f
Hank <hank@HankdeMacBook-Pro.local>
2024-03-11 22:55:28 +0800

    shits

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,12 @@
 use std::env;
 #[allow(unused_imports)]
 use std::fs;
+use std::io::BufRead;
+use std::io::BufReader;
 
+use anyhow::Context;
 use clap::{Parser, Subcommand};
+use flate2::read::ZlibDecoder;
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
@@ -15,9 +19,14 @@ struct Args {
 #[derive(Subcommand)]
 enum Command {
     Init,
+    CatFile {
+        #[clap(short = 'p')]
+        pretty_print: bool,
+        object_hash: String,
+    },
 }
 
-fn main() {
+fn main() -> anyhow::Result<()> {
     let args = Args::parse();
 
     match args.command {
@@ -28,5 +37,29 @@ fn main() {
             fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
             println!("Initialized git directory")
         }
+        Command::CatFile {
+            pretty_print,
+            object_hash,
+        } => {
+            // support shortest-unique object hashes.
+            let mut f = std::fs::File::open(format!(
+                ".git/objects/{}/{}",
+                &object_hash[..2],
+                &object_hash[2..]
+            ))
+            .context("open in .git/objects")?;
+            let mut z = ZlibDecoder::new(f);
+            let mut z = BufReader::new(z);
+            let mut buf = Vec::new();
+            z.read_until(0, &mut buf)
+                .context("read header from .git/objects")?;
+        }
     }
+
+    Ok(())
 }
+
+// cat-file command:
+// Read the Contents of the blob
+// Decompress the contents using Zlib
+// Print the content to stdout
